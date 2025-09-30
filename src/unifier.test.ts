2a5bb0c0a01b45f2ffb6055b7a12e0ed5463edfc
Antony74 <akb@akb.me.uk>
2025-09-30 17:11:16 +0100

    test: unhappy path

--- src/unifier.test.ts
+++ src/unifier.test.ts
@@ -7,4 +7,13 @@ describe(`yamma-unifier`, () => {
         const result = unifier.unify(exampleFiles['ununified.mmp']);
         expect(result).toEqual(exampleFiles['unified.mmp']);
     });
+
+    it(`throws an error given bad mmData`, () => {
+        expect(() => createUnifier(exampleFiles['bad1.mm'])).toThrow(Error);
+    });
+
+    it(`throws an error given bad mmpData`, () => {
+        const unifier = createUnifier(exampleFiles['example.mm']);
+        expect(() => unifier.unify(exampleFiles['bad1.mm'])).toThrow(Error);
+    });
 });
