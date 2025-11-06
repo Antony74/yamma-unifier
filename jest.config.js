9b8b073c7e758505886b81d1d815eefee3825b5a
Antony74 <akb@akb.me.uk>
2025-11-06 21:37:22 +0000

    Remove ts-jest as it had dependencies with deprecation warnings

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,5 @@
-const { createDefaultPreset } = require('ts-jest');
-
-const tsJestTransformCfg = createDefaultPreset().transform;
-
-/** @type {import("jest").Config} **/
 module.exports = {
     testEnvironment: 'node',
-    transform: {
-        ...tsJestTransformCfg,
-    },
     testMatch: ['<rootDir>/test/**/*.test.ts'],
     transformIgnorePatterns: ['/node_modules/(?!yamma-server)'],
 };
