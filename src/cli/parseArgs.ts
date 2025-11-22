1cb9577d28df84ac0f3646e3afe3c6241f774ace
Antony74 <akb@akb.me.uk>
2025-11-22 21:43:42 +0000

    Some progress defining args

--- src/cli/parseArgs.ts
+++ src/cli/parseArgs.ts
@@ -3,21 +3,10 @@ import { hideBin } from 'yargs/helpers';
 
 export const parseArgs = () => {
     return yargs(hideBin(process.argv))
-        .usage('yammau <mmFilename> [unify] [mmpFilenames...]')
-        .usage('yammau <mmFilename> [unify] [proofIdentifiers...]')
+        .scriptName('yamma')
+        .command('unify', 'Unify any given .mmp files')
         .positional('mmFilename', { describe: 'A .mm file', type: 'string' })
-        .positional('command', {
-            describe: 'Operation to perform',
-            type: 'string',
-            choices: ['unify', 'get', 'compress', 'uncompress', 'truncate'],
-            default: 'unify',
-        })
         .positional('mmpFilename', {
-            describe: 'Zero or more .mmp files',
-            type: 'string',
-            array: true,
-        })
-        .positional('proofIdentifiers', {
             describe: 'Zero or more .mmp files',
             type: 'string',
             array: true,
