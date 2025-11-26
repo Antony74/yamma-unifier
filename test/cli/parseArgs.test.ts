b2ee8f75adff739e7736b310cb8fc0b1e17ed662
Antony74 <akb@akb.me.uk>
2025-11-26 21:18:41 +0000

    test: unify zero, one, or two files

--- test/cli/parseArgs.test.ts
+++ test/cli/parseArgs.test.ts
@@ -5,12 +5,30 @@ type TestItem = { name: string; cmd: string; expected: Args };
 
 const testConfig: TestItem[] = [
     {
+        name: 'unify zero .mmp files',
+        cmd: 'npm start unify examples/example.mm',
+        expected: {
+            command: 'unify',
+            mmFile: 'examples/example.mm',
+            mmpFiles: [],
+        } satisfies UnifyArgs,
+    },
+    {
         name: 'unify one .mmp file',
         cmd: 'npm start unify examples/example.mm examples/ununified.mmp',
         expected: {
             command: 'unify',
             mmFile: 'examples/example.mm',
             mmpFiles: ['examples/ununified.mmp'],
+        } satisfies UnifyArgs,
+    },
+    {
+        name: 'unify two .mmp files',
+        cmd: 'npm start unify examples/example.mm examples/ununified.mmp examples/unified.mmp',
+        expected: {
+            command: 'unify',
+            mmFile: 'examples/example.mm',
+            mmpFiles: ['examples/ununified.mmp', 'examples/unified.mmp'],
         } satisfies UnifyArgs,
     },
 ];
