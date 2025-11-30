95983cd7ab4d7aa3e2a7e90a02760172f94f2553
Antony74 <akb@akb.me.uk>
2025-11-30 14:12:36 +0000

    Move proof count into parsing message

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -7,7 +7,6 @@ import { parseArgs } from './parseArgs';
 import { info } from './diagnosticsString';
 import { unify } from './unify';
 import { get } from './get';
-import { ProvableStatement } from 'yamma-server/src/mm/ProvableStatement';
 
 export const cli = async () => {
     const startTime = performance.now();
@@ -32,16 +31,8 @@ export const cli = async () => {
                 throw new Error(`Command '${command} is not implemented yet`);
         }
 
-        const proofCount = Array.from(unifier.mmParser.labelToNonSyntaxAssertionMap).filter(
-            ([_label, labeledStatement]) => {
-                return labeledStatement instanceof ProvableStatement;
-            },
-        ).length;
-
         const endTime = performance.now();
-        info(
-            `done (${prettyms(endTime - startTime)}, ${proofCount} ${proofCount === 1 ? 'proof' : 'proofs'})`,
-        );
+        info(`done (${prettyms(endTime - startTime)})`);
     } catch (e) {
         if (e instanceof Error) {
             console.error(e.message);
--- src/cli/createUnifierWithProgress.ts
+++ src/cli/createUnifierWithProgress.ts
@@ -1,8 +1,9 @@
 import { createPrompt, useState, useEffect } from '@inquirer/core';
+import * as color from 'picocolors';
 import { ProgressCallback } from 'yamma-server/src/parseNodesCreatorThread/ParseNodesCreator';
+import { ProvableStatement } from 'yamma-server/src/mm/ProvableStatement';
 import { createUnifier } from '../api/unifier';
 import { Unifier } from '../api/unifierDefinitions';
-import * as color from 'picocolors';
 
 export const createUnifierWithProgress = async (
     mmFilename: string,
@@ -10,6 +11,7 @@ export const createUnifierWithProgress = async (
 ): Promise<Unifier> => {
     const prompt = createPrompt<Unifier, {}>((_config, done): string => {
         const [progress, setProgress] = useState(0);
+        const [proofCount, setProofCount] = useState<null | number>(null);
 
         useEffect(() => {
             const progressCallback: ProgressCallback = (message) => {
@@ -21,13 +23,26 @@ export const createUnifierWithProgress = async (
             createUnifier(mmData, {
                 mm: { progressCallback },
             }).then((unifier) => {
+                setProofCount(
+                    Array.from(
+                        unifier.mmParser.labelToNonSyntaxAssertionMap,
+                    ).filter(([_label, labeledStatement]) => {
+                        return labeledStatement instanceof ProvableStatement;
+                    }).length,
+                );
+
                 setProgress(1);
                 done(unifier);
             });
         }, []);
 
+        const proofCountString =
+            proofCount === null
+                ? ''
+                : `(${proofCount} ${proofCount === 1 ? 'proof' : 'proofs'})`;
+
         return color.gray(
-            `parsing ${mmFilename}... ${Math.round(progress * 100)}%`,
+            `parsing ${mmFilename}... ${Math.round(progress * 100)}% ${proofCountString}`,
         );
     });
 
