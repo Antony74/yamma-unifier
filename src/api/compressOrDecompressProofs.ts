725309ecd5f744b4e81386b67fa0608c6ca10a37
Antony74 <akb@akb.me.uk>
2025-12-28 21:40:45 +0000

    Compress and decompress implemented.  Layout leaves something to be desired.

--- src/api/compressOrDecompressProofs.ts
+++ src/api/compressOrDecompressProofs.ts
@@ -50,22 +50,48 @@ export const compressOrDecompressProofs = (
                 const end = tokenReader.lastIndex + tokenReader.lastTokenLength;
 
                 proofsToReplace.push({ label, start, end });
-
-                // const currentProof = mmData.substring(
-                //     lastLabelStart,
-                //     tokenReader.lastIndex + tokenReader.lastTokenLength,
-                // );
-
-                // console.log(`"${currentProof}"`);
             }
         },
     );
 
     mmParser.parseFromTokenReader(tokenReader);
 
-    createUnifier(mmParser, completeConfig);
+    const unifier = createUnifier(mmParser, completeConfig);
 
-    // chunks.push(mmData.substring(start, end));
+    chunks.push(
+        mmData.substring(
+            0,
+            proofsToReplace.length ? proofsToReplace[0].start : mmData.length,
+        ),
+    );
+
+    for (let index = 0; index < proofsToReplace.length; ++index) {
+        const { label, start, end } = proofsToReplace[index];
+
+        const nextStart =
+            index + 1 < proofsToReplace.length
+                ? proofsToReplace[index + 1].start
+                : mmData.length;
+
+        const result = unifier.get(label);
+
+        const mmpProof = result.mmpUnifier.uProof;
+        const textForFormula = mmpProof?.lastMmpProofStep?.textForFormula;
+
+        if (mmpProof && mmpProof.isProofComplete && textForFormula) {
+            const mmpStatements = mmpProof.mmpStatements;
+            
+            const proofStatement = `${label} $p ${textForFormula}`;
+            const proofContent = mmpStatements[mmpStatements.length - 1].toText();
+
+            chunks.push(proofStatement);
+            chunks.push(proofContent);
+        } else {
+            chunks.push(mmData.substring(start, end));
+        }
+
+        chunks.push(mmData.substring(end, nextStart));
+    }
 
     return Buffer.concat(chunks.map(Buffer.from)).toString();
 };
