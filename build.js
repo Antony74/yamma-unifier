9d498e77911ecd3f64dcd0feb06461203b44da71
Antony74 <akb@akb.me.uk>
2026-01-02 17:00:45 +0000

    Make both builds (code in esbuild and types in TypeScript) pass

--- build.js
+++ build.js
@@ -1,6 +1,11 @@
+const path = require('path');
+
 require('esbuild').build({
     entryPoints: ['src/index.ts'],
     bundle: true,
     outdir: 'dist',
     platform: 'node',
+    alias: {
+        'yamma-server': path.resolve('yamma/server'),
+    },
 });
