883521acedc4274af37c62c9a79648f19b96293e
Antony74 <akb@akb.me.uk>
2026-01-03 23:06:29 +0000

    test: coverage.  It can unify from parsers and a single thread

--- test/unifier.test.ts
+++ test/unifier.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest';
-import { createUnifier } from '../src/unifier';
+import { createUnifier, parseMm, parseMmp } from '../src/unifier';
 import { exampleFiles } from './examples';
 
 describe(`yamma-unifier`, () => {
@@ -7,6 +7,15 @@ describe(`yamma-unifier`, () => {
         const unifier = createUnifier(exampleFiles['example.mm']);
         await unifier.deepParse();
         const result = unifier.unify(exampleFiles['ununified.mmp']);
+        expect(result.text).toEqual(exampleFiles['unified.mmp']);
+    });
+
+    it(`can unify from parsers and a single thread`, async () => {
+        const mmParser = parseMm(exampleFiles['example.mm']);
+        const unifier = createUnifier(mmParser, { mm: { singleThread: true } });
+        await unifier.deepParse();
+        const mmpParser = parseMmp(exampleFiles['ununified.mmp'], mmParser);
+        const result = unifier.unify(mmpParser);
         expect(result.text).toEqual(exampleFiles['unified.mmp']);
     });
 
